6633bcb2cc5f44145579f91b3ee49bec9d5ef2ee
AADI KRISHNA <aadikrishna04@gmail.com>
2023-04-28 02:33:29 -0700

    first commit

---
+++ client/src/components/Copyright.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Copyright = () => {
+  return (
+    <Typography variant="subtitle1" color="text.secondary">
+      Copyright © 2023{" "} 
+        — ProConnect
+    </Typography>
+  );
+};
+
+export default Copyright;
---
+++ client/src/components/views/ExploreView.js
@@ -0,0 +1,36 @@
+import { Button, Card, Container, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { getPosts } from "../../api/posts";
+import { isLoggedIn } from "../../helpers/authHelper";
+import CreatePost from "../CreatePost";
+import GridLayout from "../GridLayout";
+import Loading from "../Loading";
+import Navbar from "../Navbar";
+import SortBySelect from "../SortBySelect";
+import PostCard from "../PostCard";
+import Sidebar from "../Sidebar";
+import HorizontalStack from "../util/HorizontalStack";
+import PostBrowser from "../PostBrowser";
+import { Link, useNavigate } from "react-router-dom";
+
+const ExploreView = () => {
+  const navigate = useNavigate();
+  if (isLoggedIn()) {
+    navigate("/")
+  } else {
+    window.location.href = "/login";
+  }
+
+  return (
+  <Container>
+    <Navbar />
+    <GridLayout
+      left={<PostBrowser createPost contentType="posts" />}
+      right={<Sidebar />}
+    />
+  </Container>
+);
+};
+
+export default ExploreView;
